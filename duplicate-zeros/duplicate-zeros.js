43509b0697b86c7a11c8b3c99bf986ba754b2320
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-05-18 17:11:39 +0300

    Time: 83 ms (25.05%) | Memory: 44.3 MB (42.04%) - LeetSync

--- duplicate-zeros/duplicate-zeros.js
+++ duplicate-zeros/duplicate-zeros.js
@@ -3,8 +3,8 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function(arr) {
-    for(let i = 0; i<arr.length;i++){
-        if(arr[i]===0){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] === 0){
             arr.pop(); 
             arr.splice(i+1,0,0);
             i++;
