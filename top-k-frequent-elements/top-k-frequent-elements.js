cac7c97411b4c6d046ddc963e6ddd5399c2a477f
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-05-22 17:34:41 +0300

    Time: 83 ms (37.11%) | Memory: 44.9 MB (75.68%) - LeetSync

---
+++ top-k-frequent-elements/top-k-frequent-elements.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let result = [];
+    let numbers = {};
+
+    for(let i=0; i<nums.length; i++) {
+        if(numbers[nums[i]]) {
+            numbers[nums[i]]++;
+        } else {
+            numbers[nums[i]] = 1;
+        }
+    }
+
+    let arr = Object.entries(numbers).sort((a, b) => {
+        return a[1] - b[1]
+    });
+
+    while(result.length != k) {
+        let curr = arr.pop();
+        result.push(curr[0]);
+    }
+
+    return result;
+};
