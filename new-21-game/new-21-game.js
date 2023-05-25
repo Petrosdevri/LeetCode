98a17bdaeb3617eb9b29e86a63743dba3b2bc0bd
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-05-25 19:59:21 +0300

    Time: 68 ms (50.00%) | Memory: 44.4 MB (50.00%) - LeetSync

---
+++ new-21-game/new-21-game.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number} maxPts
+ * @return {number}
+ */
+var new21Game = function(n, k, maxPts) {
+    let dp = Array(n+1).fill(0);
+    dp[0] = 1;
+    let s = k > 0 ? 1 : 0;
+
+    for(let i=1; i<=n; i++) {
+        dp[i] = s/maxPts;
+        if(i < k) {
+            s += dp[i];
+        }
+        if(i - maxPts >= 0 && i - maxPts < k) {
+            s -= dp[i-maxPts];
+        }
+    }
+    let ans = 0;
+    for(let i=k; i<=n; i++) {
+        ans += dp[i];
+    }
+    return ans;
+};
