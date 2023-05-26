bcc9c21da046a5e7cb7ea6ef4bab0057be18a450
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-05-26 18:01:34 +0300

    Time: 225 ms (26.24%) | Memory: 57 MB (19.79%) - LeetSync

---
+++ number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var numSubseq = function(nums, target) {
+    let pow = [];
+    pow.push(1);
+
+    nums.sort((a,b)=>{return a-b;})
+
+    const mod = 1e9+7;
+
+    for(let i=1; i<nums.length; i++){
+        pow.push((pow[pow.length-1]*2) % mod);
+    }
+
+    let i = 0;
+    let j = nums.length-1;
+    let res = 0;
+
+    while(i <= j) {
+        if(nums[i]+nums[j] > target){
+            j--;
+        }
+        else{
+            res = (res+pow[j-i++]) % mod;
+        }
+    }
+
+    return res;
+};
