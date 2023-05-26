0f14aae1a637281efa399674e06124c777b7729b
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-05-26 17:59:07 +0300

    Time: 74 ms (23.63%) | Memory: 43.4 MB (95.71%) - LeetSync

---
+++ reverse-bits/reverse-bits.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    let result = 0b0;
+    let current = n;
+
+    for (let i = 0; i < 32; i++) {
+        let lastBit = current & 0b1;
+        result = result << 1;
+        result = result | lastBit;
+        current = current >> 1;
+    }
+
+    return result >>> 0;
+};
