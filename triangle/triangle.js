137cc7ba40e662c739098e7a7991d5768d60133e
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-05-26 17:57:37 +0300

    Time: 55 ms (94.14%) | Memory: 44.3 MB (20.71%) - LeetSync

---
+++ triangle/triangle.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} triangle
+ * @return {number}
+ */
+var minimumTotal = function(triangle) {
+    let n = triangle.length;
+    let dp = Array(n).fill(Number.MAX_VALUE).map(()=> Array(n).fill(Number.MAX_VALUE));
+    //let min = Number.MAX_VALUE;
+    function f(i, j){
+        if(i == n-1){return triangle[i][j];}
+        if(dp[i][j] != Number.MAX_VALUE) return dp[i][j];
+        let down = triangle[i][j] + f(i+1, j);
+        let diag = triangle[i][j] + f(i+1, j+1);
+        return dp[i][j] = Math.min(down, diag);
+    }
+    return f(0, 0);
+};
