59d65e840a3341e192824c6ec207a07fe32bd281
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-05-27 21:22:17 +0300

    Time: 105 ms (36.21%) | Memory: 76.2 MB (37.47%) - LeetSync

---
+++ convert-object-to-json-string/convert-object-to-json-string.ts
@@ -0,0 +1,23 @@
+function jsonStringify(object: any): string {
+    switch(typeof object) {
+        case 'object':
+            if(Array.isArray(object)) {
+                const elements = object.map((element) => jsonStringify(element));
+                return `[${elements.join(',')}]`;
+            } else if(object) {
+                const keys = Object.keys(object);
+                const keyValuePairs = keys.map(key =>
+                    `"${key}":${jsonStringify(object[key])}`);
+                return `{${keyValuePairs.join(',')}}`;
+            } else {
+                return 'null';
+            }
+        case 'boolean':
+        case 'number':
+            return `${object}`;
+        case 'string':
+            return `"${object}"`;
+        default:
+            return '';
+    }
+};
