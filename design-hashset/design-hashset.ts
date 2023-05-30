6dc05e43ea6bf0a7ab95128b2b1eb4c2c89534e2
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-05-30 12:23:11 +0300

    Time: 1224 ms (5.19%) | Memory: 56.7 MB (50.65%) - LeetSync

---
+++ design-hashset/design-hashset.ts
@@ -0,0 +1,57 @@
+type HashNode<T=number> = {
+    value: T;
+    next?: HashNode<T>;
+}
+
+class MyHashSet {
+    private head?: HashNode;
+
+    constructor() {
+        this.head = undefined;
+    }
+
+    add(key: number): void {
+        const node: HashNode = { value: key };
+        if(!this.head) {
+            this.head = node;
+            return;
+        } if(this.contains(key)) return;
+        node.next = this.head;
+        this.head = node;
+    }
+
+    remove(key: number): void {
+        if(!this.head) return;
+        if(this.head.value === key) {
+            this.head = this.head.next;
+            return;
+        }
+        let curr = this.head;
+        while(curr?.next) {
+            if(curr.next.value === key) {
+                curr.next = curr.next.next;
+                break;
+            }
+            curr = curr.next;
+        }
+    }
+
+    contains(key: number): boolean {
+        let curr = this.head;
+        while(curr) {
+            if(curr.value === key) {
+                return true;
+            }
+            curr = curr.next;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * var obj = new MyHashSet()
+ * obj.add(key)
+ * obj.remove(key)
+ * var param_3 = obj.contains(key)
+ */
