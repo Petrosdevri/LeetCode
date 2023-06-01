1ea8d96221aaee228789575533125f2f589ebec7
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-06-01 16:22:46 +0300

    Time: 139 ms (44.07%) | Memory: 50.1 MB (72.88%) - LeetSync

---
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.ts
@@ -0,0 +1,36 @@
+function shortestPathBinaryMatrix(grid: number[][]): number {
+    const directions = [
+		[1, 0],
+		[-1, 0],
+		[0, 1],
+		[0, -1],
+		[1, 1],
+		[1, -1],
+		[-1, 1],
+		[-1, -1],
+	];
+
+    if(grid[0][0] == 1) return -1;
+
+    const n = grid.length;
+    const queue = [[0, 0, 1]];
+
+    while(queue.length) {
+        const [row, col, path] = queue.shift();
+        if(row == n-1 && col == n-1) return path;
+
+        for(const [dx, dy] of directions) {
+            let x = row+dx;
+            let y = col+dy;
+
+            if(x < 0 || x >= n) continue;
+            if(y < 0 || y >= n) continue;
+            if(grid[x][y] != 0) continue;
+
+            queue.push([x, y, path+1]);
+            grid[x][y] = 1; 
+        }
+    }
+     
+    return -1;
+};
