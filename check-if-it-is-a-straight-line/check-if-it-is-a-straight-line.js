a0d232ee15c5d0b686677e420c0b06a0f7cad5de
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-06-05 21:46:15 +0300

    Time: 67 ms (38.16%) | Memory: 44.1 MB (59.90%) - LeetSync

---
+++ check-if-it-is-a-straight-line/check-if-it-is-a-straight-line.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function(coordinates) {
+    if(coordinates.length == 2) return true;
+
+    const getDX = (a, b) => {
+        return a[0] - b[0];
+    };
+
+    const getDY = (a, b) => {
+        return a[1] - b[1];
+    };
+
+    let dx = getDX(coordinates[1], coordinates[0]);
+    let dy = getDY(coordinates[1], coordinates[0]);
+
+    for(let i=2; i<coordinates.length; i++) {
+        if(dx*getDY(coordinates[i], coordinates[0]) != dy*getDX(coordinates[i], coordinates[0])) return false;
+    }
+
+    return [dx, dy];
+};
