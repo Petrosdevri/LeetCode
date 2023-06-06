37ff392e622db9bbeed1f043d283f86166304c09
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-06-06 20:28:46 +0300

    Time: 55 ms (85.03%) | Memory: 41.8 MB (92.37%) - LeetSync

--- can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.js
+++ can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.js
@@ -4,10 +4,9 @@
  */
 var canMakeArithmeticProgression = function(arr) {
     arr = arr.sort((a, b) => a - b);
-    let commonDifference = arr[1] - arr[0]
-    for(let i = 2; i < arr.length; i++){
-        if(arr[i] - arr[i-1] !== commonDifference) return false
+    let diff = arr[1] - arr[0];  
+    for(let i=2; i<arr.length; i++) {
+        if(diff != arr[i]-arr[i-1]) return false;
     }
-
-    return true
+    return true;
 };
