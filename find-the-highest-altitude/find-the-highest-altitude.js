0900794ca391b4b09c6e4fe498fcdd07101fc5c3
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-06-19 20:24:11 +0300

    Time: 60 ms (46.86%) | Memory: 42 MB (47.17%) - LeetSync

---
+++ find-the-highest-altitude/find-the-highest-altitude.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    let maxCount = 0;
+    let currentCount = 0;
+    for(let altitude of gain) {
+        currentCount += altitude;
+        if(currentCount > maxCount) {
+            maxCount = currentCount;
+        }
+    }
+
+    return maxCount;
+};
