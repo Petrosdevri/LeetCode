7330d96c0e3fc83ff22b1ea4018bee5a8f2ef4e1
Petros Devrikis <100411628+Petrosdevri@users.noreply.github.com>
2023-06-19 20:36:00 +0300

    Time: 57 ms (94.89%) | Memory: 42.3 MB (27.76%) - LeetSync

---
+++ add-two-promises/add-two-promises.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    try {
+        return await promise1 + await promise2;
+    } catch(err) {
+        console.error(err)
+    }
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
